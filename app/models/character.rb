2992191fdf77332c228dceb9deba7caf77e53858
j-shilling <shilling.jake@gmail.com>
2018-08-31 09:51:42 -0400

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,14 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    name + " always says: " + catchphrase
+  end
+
+  def build_show(args)
+    show = Show.create(args)
+    update(show: show)
+    show
+  end
 end
